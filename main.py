626e4983237addeba446dff22d1db3b05efd892e
norsultani <norsultani@gmail.com>
2025-12-26 13:34:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ df.head()
 selected_features = df[["MDVP:Jitter(%)", "MDVP:Flo(Hz)"]]
 
 x = selected_features
-y = df["MDVP:RAP"]
+y = df["status"]
 
 x
 
-scaler = MinMaxScaler()
+caler = MinMaxScaler()
 x_scales = scaler.fit_transform(x)
 
 
@@ -24,10 +24,12 @@ x_train, x_test, y_train, y_test = train_test_split(x_scales, y, test_size = 0.2
 
 len(x_train), len(x_test), len(y_train), len(y_test)
 
-from sklearn.neighbors import KNeighborsRegressor
 
 
-model = KNeighborsRegressor(n_neighbors = 3)
+from sklearn.neighbors import KNeighborsClassifier
+
+
+model = KNeighborsClassifier(n_neighbors = 3)
 
 
 model.fit(x_train, y_train)
@@ -37,13 +39,12 @@ yd_pred = model.predict(x_test)
 yd_pred
 
 
-from sklearn.metrics import r2_score
+from sklearn.metrics import accuracy_score
 
-r_squared = r2_score(y_test, yd_pred)
+accuracy = accuracy_score(y_test, yd_pred)
 
-print(f"R-squared: {r_squared:.4f}")
-
+print(accuracy)
 
 import joblib
 
-joblib.dump(model, 'parkinsons_predict.joblib')
+joblib.dump(model, 'parkinsons_predict2.joblib')
